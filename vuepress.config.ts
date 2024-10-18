fcf55a845dee0cd7f87fd81be559a466e0553ac3
Power Lin <linyuxuanlin@outlook.com>
2024-10-18 21:58:07 +0800

    Update vuepress.config.ts

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -21,7 +21,7 @@ export default defineUserConfig({
   lang: 'zh-CN',
   public: `./public`,
   // 网站标题，标题颜色可在 src/client/css/main.css 中修改
-  title: 'FList',
+  title: 'File_host',
   // 网站的简介，有助于搜索引擎收录
   description: 'FList - 将 GitHub Releases 以类似网盘的形式展示在网页上，方便用户下载开源软件。 支持视频、音频、图片、PDF 等文件的在线预览。',
   // 页面 <head> 标签内添加的额外标签。 不要修改/logo.png可以替换掉这个文件，删除logo.png会导致构建出错。
@@ -32,7 +32,7 @@ export default defineUserConfig({
   // 主题配置 FileList 是 vuepress 的一个主题，文件展示的功能全部由这个主题提供。
   theme: FileList([
     {
-      mountPath: "/ProgrammingVTuberLogos",
+      mountPath: "/",
       analysis: githubReposAnalysis({
         user: "Aikoyori",
         repository: "ProgrammingVTuberLogos",
